4d1c6050522f4a10a40934cef0ac84163a63b836
Matt Wright <matthewbwright@gmail.com>
2019-10-22 09:40:58 -0700

    Updates project for the day's work

---
+++ MVCMovie/MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // GET: HelloWorld/Welcome
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
