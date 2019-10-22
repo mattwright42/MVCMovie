a8a9e46090a07e9c757c4c4db7483927c5133fe1
Matt Wright <matthewbwright@gmail.com>
2019-10-22 11:31:06 -0700

    Adjusts formatting of the date display

--- MVCMovie/MVCMovie/Models/Movie.cs
+++ MVCMovie/MVCMovie/Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCMovie.Models
 {
@@ -10,6 +11,9 @@ namespace MVCMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
